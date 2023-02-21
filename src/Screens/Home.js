717bb5fc4989ad605801354445c2e368b427a9b2
TomAmericana <fake.tom@hotmail.com>
2023-02-21 14:40:51 -0600

    RE: Update

---
+++ src/Screens/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+
+const Home = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    );
+}
+
+export default Home;
+
