0af80db43a005401598aba76a42376a82018e110
TomAmericana <fake.tom@hotmail.com>
2023-02-21 14:41:14 -0600

    Re: Update

--- src/Screens/Scanner.js
+++ src/Screens/Scanner.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
+import { Text, View, StyleSheet, Button, Linking } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 
 export default function Scan() {
@@ -21,7 +21,7 @@ export default function Scan() {
   // Función que gestionara el comportamiento cuando se detecte un código, en este caso establecer una variable de estado que le dice a la app que ya se ha escaneado un código y mostrar un alert con los datos del mismo
   const handleBarCodeScanned = ({ type, data }) => {
     setScanned(true);
-    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+    alert(`Bar code with type ${type} and data ${Linking.openURL('${data}')} has been scanned!`);
   };
 
   //Validaciones que nos permitan determinar que hacer si el permiso de la cámara no se ha obtenido aún o ha sido denegado
