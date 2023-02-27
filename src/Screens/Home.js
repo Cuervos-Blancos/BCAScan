0d49eaf5f705d69a9451eee54bcef5572c084500
TomAmericana <fake.tom@hotmail.com>
2023-02-27 16:57:59 -0600

    Re: Uptade

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -4,10 +4,9 @@ import {View, StyleSheet, Text} from 'react-native';
 const Home = () => {
     return (
         <View>
-            <Text></Text>
+            
         </View>
     );
 }
 
 export default Home;
-
