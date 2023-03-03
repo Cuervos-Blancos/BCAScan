81610021d2c0cc19be6a80447fc0b4f72212b552
TomAmericana <fake.tom@hotmail.com>
2023-03-03 13:59:26 -0600

    Re: Update

--- src/Screens/TecladoScreen.js
+++ src/Screens/TecladoScreen.js
@@ -30,6 +30,7 @@ export default function TecladoScreen() {
         setFirstNumber("");
     };
 
+    // Declarando la constante para imprimir la cadena de texto; ajustando el tamano de letra acorde a los digitos tipados
     const firstNumberDisplay = () => {
         if (firstNumber && firstNumber.length < 6) {
             return <Text style={Styles.screenFirstNumber}>{firstNumber}</Text>;
@@ -71,33 +72,33 @@ export default function TecladoScreen() {
             </View>
             <View style={Styles.row}>
                 <Button title="C" isGray onPress={clear} />
-                <Button title="+/-" isGray onPress={() => handleOperationPress("+/-")} />
+                {/* <Button title="+/-" isGray onPress={() => handleOperationPress("+/-")} />
                 <Button title="％" isGray onPress={() => handleOperationPress("％")} />
-                <Button title="÷" isBlue onPress={() => handleOperationPress("/")} />
+                <Button title="÷" isBlue onPress={() => handleOperationPress("/")} /> */}
             </View>
             <View style={Styles.row}>
                 <Button title="7" onPress={() => handleNumberPress("7")} />
                 <Button title="8" onPress={() => handleNumberPress("8")} />
                 <Button title="9" onPress={() => handleNumberPress("9")} />
-                <Button title="×" isBlue onPress={() => handleOperationPress("*")} />
+                {/* <Button title="×" isBlue onPress={() => handleOperationPress("*")} /> */}
             </View>
             <View style={Styles.row}>
                 <Button title="4" onPress={() => handleNumberPress("4")} />
                 <Button title="5" onPress={() => handleNumberPress("5")} />
                 <Button title="6" onPress={() => handleNumberPress("6")} />
-                <Button title="-" isBlue onPress={() => handleOperationPress("-")} />
+                {/* <Button title="-" isBlue onPress={() => handleOperationPress("-")} /> */}
             </View>
             <View style={Styles.row}>
                 <Button title="1" onPress={() => handleNumberPress("1")} />
                 <Button title="2" onPress={() => handleNumberPress("2")} />
                 <Button title="3" onPress={() => handleNumberPress("3")} />
-                <Button title="+" isBlue onPress={() => handleOperationPress("+")} />
+                {/* <Button title="+" isBlue onPress={() => handleOperationPress("+")} /> */}
             </View>
             <View style={Styles.row}>
-                <Button title="." onPress={() => handleNumberPress(".")} />
+                {/* <Button title="." onPress={() => handleNumberPress(".")} /> */}
                 <Button title="0" onPress={() => handleNumberPress("0")} />
                 <Button title="⌫" onPress={() => setFirstNumber(firstNumber.slice(0, -1))} />
-                <Button title="=" isBlue onPress={() => getResult()} />
+                {/* <Button title="=" isBlue onPress={() => getResult()} /> */}
             </View>
         </View>
     );
