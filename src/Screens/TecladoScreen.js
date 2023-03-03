bf8edb5d09710d1d198b9724033aeacdc1d66c98
Pain <64872267+YorkYo@users.noreply.github.com>
2023-03-02 18:36:59 -0600

    Re: Update

--- src/Screens/TecladoScreen.js
+++ src/Screens/TecladoScreen.js
@@ -1,27 +1,104 @@
 import React from 'react';
-import { View, StyleSheet, Button } from 'react-native';
+import { View, StyleSheet, Button, Text } from 'react-native';
 
-import { Styles } from '../Components/Styles';
+import { Styles } from '../Styles/GlobalStyles';
+import { myColors } from '../Styles/Colors';
+import Button from '../Styles/Button';
 
-const TecladoScreen = () => {
+
+
+export default function TecladoScreen() {
+    const [firstNumber, setFirstNumber] = React.useState("");
+    // const [secondNumber, setSecondNumber] = React.useState("");
+    // const [operation, setOperation] = React.useState("");
+    // const [result, setResult] = React.useState < Number | null > (null);
+
+    // Iniciamos asignando el numero tipado a la cadena de texto
+    const handleNumberPress = (buttonValue: string) => {
+        if (firstNumber.length < 15) {
+            setFirstNumber(firstNumber + buttonValue);
+        }
+    };
+
+    // const handleOperationPress = (buttonValue: string) => {
+    //     setOperation(buttonValue);
+    //     setSecondNumber(firstNumber);
+    //     setFirstNumber("");
+    // };
+
+    const clear = () => {
+        setFirstNumber("");
+    };
+
+    const firstNumberDisplay = () => {
+        if (firstNumber && firstNumber.length < 6) {
+            return <Text style={Styles.screenFirstNumber}>{firstNumber}</Text>;
+        }
+        if (firstNumber === "") {
+            return <Text style={Styles.screenFirstNumber}>{"0"}</Text>;
+        }
+        if (firstNumber.length > 5 && firstNumber.length < 8) {
+            return (
+                <Text style={[Styles.screenFirstNumber, { fontSize: 70 }]}>
+                    {firstNumber}
+                </Text>
+            );
+        }
+        if (firstNumber.length > 7) {
+            return (
+                <Text style={[Styles.screenFirstNumber, { fontSize: 50 }]}>
+                    {firstNumber}
+                </Text>
+            );
+        }
+    };
+
     return (
-        <View>
+        <View style={Styles.viewBottom}>
+            <View
+                style={{
+                    height: 120,
+                    width: "90%",
+                    justifyContent: "flex-end",
+                    alignSelf: "center",
+                }}
+            >
+                {/* <Text style={Styles.screenSecondNumber}>
+                    {secondNumber}
+                    <Text style={{ color: "purple", fontSize: 50, fontWeight: '500' }}>{operation}</Text>
+                </Text> */}
+                {firstNumberDisplay()}
+            </View>
             <View style={Styles.row}>
-                <Button title="C" onPress={() => handleOperationPress("+/-")} />
-                <Button title="+/-" onPress={() => handleOperationPress("+/-")} />
-                <Button title="％" onPress={() => handleOperationPress("％")} />
-                <Button title="÷" onPress={() => handleOperationPress("/")} />
+                <Button title="C" isGray onPress={clear} />
+                <Button title="+/-" isGray onPress={() => handleOperationPress("+/-")} />
+                <Button title="％" isGray onPress={() => handleOperationPress("％")} />
+                <Button title="÷" isBlue onPress={() => handleOperationPress("/")} />
             </View>
             <View style={Styles.row}>
                 <Button title="7" onPress={() => handleNumberPress("7")} />
                 <Button title="8" onPress={() => handleNumberPress("8")} />
                 <Button title="9" onPress={() => handleNumberPress("9")} />
-                <Button title="×" onPress={() => handleOperationPress("*")} />
+                <Button title="×" isBlue onPress={() => handleOperationPress("*")} />
+            </View>
+            <View style={Styles.row}>
+                <Button title="4" onPress={() => handleNumberPress("4")} />
+                <Button title="5" onPress={() => handleNumberPress("5")} />
+                <Button title="6" onPress={() => handleNumberPress("6")} />
+                <Button title="-" isBlue onPress={() => handleOperationPress("-")} />
+            </View>
+            <View style={Styles.row}>
+                <Button title="1" onPress={() => handleNumberPress("1")} />
+                <Button title="2" onPress={() => handleNumberPress("2")} />
+                <Button title="3" onPress={() => handleNumberPress("3")} />
+                <Button title="+" isBlue onPress={() => handleOperationPress("+")} />
+            </View>
+            <View style={Styles.row}>
+                <Button title="." onPress={() => handleNumberPress(".")} />
+                <Button title="0" onPress={() => handleNumberPress("0")} />
+                <Button title="⌫" onPress={() => setFirstNumber(firstNumber.slice(0, -1))} />
+                <Button title="=" isBlue onPress={() => getResult()} />
             </View>
         </View>
     );
 }
-
-const styles = StyleSheet.create({})
-
-export default TecladoScreen;
