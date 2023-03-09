73460d963673c42bbb5a7f56e287a08d55de3e98
TomAmericana <fake.tom@hotmail.com>
2023-03-09 15:33:57 -0600

    Update

--- src/Screens/AlumnoCard.js
+++ src/Screens/AlumnoCard.js
@@ -1,14 +1,64 @@
-import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+import GetAlumno from "../Hooks/GetAlumno";
 
 const AlumnoCard = () => {
+    const [Alumnos, setAlumnos] = useState(false);
+
+    const [result, GetInfoAlumnos] = GetAlumno();
+
+    const [colums, setColums] = useState(1);
+
+    useEffect(() => {
+        GetInfoAlumnos();
+    }, []);
+
+    useEffect(() => {
+        setAlumnos(result);
+    }, [result]);
+
+    if (result == false) {
+        return (
+            <View style={{ justifyContent: "center", alignItems: "center", flex: 1 }}>
+                <Text style={{ fontSize: 20, color: "gray" }}>Cargando...</Text>
+            </View>
+        );
+    }
+
     return (
-        <View>
-            // Grid
+        <View style={styles.container}>
+            <Search
+                alumnos={result}
+                setAlumnos={setAlumnos}
+                setColums={setColums}
+                colums={colums}
+            />
+            {/* <ListaPokemones pokemones={Pokemons} colums={colums} /> */}
+            <ListaAlumnos alumnos={Alumnos} colums={colums} />
         </View>
     );
-}
+};
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+});
 
 export default AlumnoCard;
+
+// import React from 'react';
+// import {View, StyleSheet} from 'react-native';
+
+// const AlumnoCard = () => {
+//     return (
+//         <View>
+//             // Grid
+//         </View>
+//     );
+// }
+
+// const styles = StyleSheet.create({})
+
+// export default AlumnoCard;
